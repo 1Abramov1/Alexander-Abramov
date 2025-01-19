88773ceb74680f83c9064e6a6e4d9cb64a7f5427
Alexander Abramov <richesantiques9@gmail.com>
2025-01-19 15:26:54 +0400

    the process of creating card number masking functions has been completed

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,28 @@
-
 """
 Написать функцию, которая, обрабатывает информацию о карте и о счете
 """
 
-def mask_account_card
+import sys
+sys.path.append('C:/Users/New_Alexs/PycharmProjects/PythonProject1')
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(number: str) -> str:
+    """Функция, маскировки счета/номера карты"""
+
+    if len (number.split()[-1]) == 16:
+       new_number = get_mask_card_number(number.split()[-1])
+       result = f"{number[:-16]}{new_number}"
+       return result
+    elif len (number.split()[-1]) == 20:
+       new_number = get_mask_account(number.split()[-1])
+       result = f"{number[:-20]}{new_number}"
+       return result
+    else:
+        print("Введен некорректный номер счета")
+
+
+def get_new_data(old_data: str) -> str:
+    """Функция принимает строку и выводит дату в формате dd.mm.yyyy"""
+    data_slize = old_data [0:10].split("-")
+    return ".".join(data_slize[::-1])
