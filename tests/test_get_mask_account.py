18b7fd3c5252e620197bb83aa2cff4169b7f1188
Alexander Abramov <richesantiques9@gmail.com>
2025-02-19 14:03:56 +0400

    testing files were created and tested, the processing.py module was created

---
+++ tests/test_get_mask_account.py
@@ -0,0 +1,26 @@
+from src.masks import get_mask_account
+import pytest
+
+
+@pytest.fixture
+def account_number():
+  return "73654108430135874305" # стандартная длина
+
+
+def test_get_mask_account(account_number):
+  masked_account = get_mask_account(account_number)
+  assert masked_account == "**4305"
+
+
+@pytest.mark.parametrize("account_number, expected", [
+ ("44811934567890122392", "**2392"),  # стандартный случай
+ ("1232", "**1232"), # минимальная длина
+ ("","**" ), # пустая строка
+ ("1234567890123456789021", "**9021"),  # нестандартная длина
+ ("abcd1234abcd5678bn2f", "**bn2f")  # нестандартный формат
+])
+
+
+def test_get_mask_account1(account_number, expected):
+  masked_account = get_mask_account(account_number)
+  assert masked_account == expected
--- tests/test_get_mask_card_number.py
+++ tests/test_get_mask_card_number.py
@@ -1,3 +1,4 @@
+from src.masks import get_mask_card_number
 import pytest
 
 @pytest.fixture
@@ -10,17 +11,15 @@ def test_get_mask_card_number(card_number):
   assert masked_number == "1234 56** **** 5678"
 
 
-import pytest
-
 @pytest.mark.parametrize("card_number, expected", [
  ("1234567812345678", "1234 56** **** 5678"),  # стандартный случай
- ("1234", "1234"), # минимальная длина
- ("","" ), # пустая строка
+ ("1234", "1234 ** **** 1234"), # минимальная длина
+ (""," ** **** " ), # пустая строка
  ("12345678901234567890", "1234 56** **** 7890"),  # нестандартная длина
  ("abcd1234abcd5678", "abcd 12** **** 5678")  # нестандартный формат
 ])
 
 
-def test_get_mask_card_number(card_number, expected):
+def test_get_mask_card_number1(card_number, expected):
   masked_number = get_mask_card_number(card_number)
   assert masked_number == expected
