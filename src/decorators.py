f7bbf8f55547979360be06b79430e212d578a5b7
Alexander Abramov <richesantiques9@gmail.com>
2025-03-27 23:16:05 +0400

    copied the decorators.py file from the homework_11_2 branch

---
+++ src/decorators.py
@@ -0,0 +1,41 @@
+from functools import wraps
+from typing import Any
+from typing import Callable
+from typing import Union
+
+
+def log(filename: Any = None) -> Callable:
+    """Декоратор для логирования функции, аргументов, результатов и ошибок"""
+
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} ok: {result}"
+                if filename:
+                    with open(filename, "a") as log_file:
+                        log_file.write(log_message + "\n")
+                else:
+                    print(log_message)
+                return result
+            except Exception as e:
+                error_message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a") as log_file:
+                        log_file.write(error_message + "\n")
+                else:
+                    print(error_message)
+                raise
+
+        return wrapper
+
+    return decorator
+
+
+@log("mylog.txt")
+def my_decorator(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a * b
+
+
+print(my_decorator(3, 5))
