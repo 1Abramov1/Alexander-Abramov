4cbb4f112e9e545c9f7292663d80ccd465f28a47
Alexander Abramov <richesantiques9@gmail.com>
2025-04-24 00:17:17 +0400

    files are checked and working

---
+++ src/reading_csv_and_excel.py
@@ -0,0 +1,44 @@
+import csv
+from typing import Any
+from typing import Dict
+from typing import Hashable
+
+import pandas as pd
+
+"""Функция для чтения CSV-файла и преобразования его в список словарей"""
+
+
+def read_transact_csv_file() -> list[Dict[str, str]]:
+    transactions = []
+    with open("../data/transactions.csv", encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            if row["state"].strip() == "EXECUTED":
+                transactions.append(row)
+        print(len(transactions))
+        return transactions
+
+
+filtered_transactions = read_transact_csv_file()  # преобразование выводимых данных из файла сsv
+for transact in filtered_transactions:
+    print(
+        f"ID: {transact['id']}, State: {transact['state']},"
+        f"Date: {transact['date']}, Amount: {transact['amount']}, Currency_name: {transact['currency_name']}"
+        f" Currency_code: {transact['currency_code']}, From: {transact['from']}, "
+        f"To: {transact['to']}, Description: {transact['description']}"
+    )
+    print("-" * 240)
+
+
+"""Функция для чтения Excel-файла и преобразования его в список словарей"""
+
+
+def read_transact_excel_file(file_path: str) -> list[dict[Hashable, Any]]:
+    return pd.read_excel(file_path, engine="openpyxl").to_dict(orient="records")
+
+
+# Вызов функции и вывод результата
+result = read_transact_excel_file("../data/transactions_excel.xlsx")
+for transaction in result:  # type: dict[Hashable, Any]
+    print(transaction)
+    print("-" * 50)  # разделитель между записями
